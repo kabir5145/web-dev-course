dbf3eea787c53f5907bc9e9e2e019a74b4b7d717
kabir5145 <kabir10112006@gmail.com>
2025-11-24 16:38:04 +0530

    video 55 of web dev course

---
+++ video 55/script.js
@@ -0,0 +1,28 @@
+console.log("Hey this is tutorial 55 ")
+
+let a = 5;
+let b = 6;
+let c = "kabir";
+
+// var 55a = "Harry"; // invalid variable name
+
+// console.log("The sum of a and b is ", a + b);
+// console.log(typeof a , typeof b , typeof c);
+
+{
+    let a = 9;
+    console.log(a)
+}
+console.log(a)
+// const a1 = 56;
+// a1 = a + 2; // we cannot change the value of constant variable
+
+let x = "Kabir";
+let y = 22;
+let z = 22.5;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x , y , z , p , q , r);
+console.log(typeof x , typeof y , typeof z , typeof p , typeof q , typeof r);
