7dbd3023ba81c0803d53c3193347e2a3b6191d9b
kabir5145 <kabir10112006@gmail.com>
2025-12-07 20:16:36 +0530

    maked a faulty calculator

--- video 59/index.js
+++ video 59/index.js
@@ -10,3 +10,39 @@ This falulty calculator does following :
 
 It performs wrong operations 10% of the time.
 */
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+function faultyCalculator(num1, num2, operator) {
+    const randomChance = Math.random();
+    const faultProbability = 0.1; // 10% chance of fault
+    if (randomChance < faultProbability) {
+        switch (operator) {
+            case '+':
+                return num1 - num2; // Faulty addition
+            case '*':
+                return num1 + num2; // Faulty multiplication
+            case '/':
+                return num1 ** num2; // Faulty division
+            default:
+                return 'Invalid operator';
+        }   
+    } else {
+        switch (operator) { 
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                return num1 / num2;
+            default:
+                return 'Invalid operator';
+        }
+    }
+}
